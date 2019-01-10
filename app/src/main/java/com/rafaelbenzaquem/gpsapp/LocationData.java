163153d6d765b36ae6a8c7660805462a0d6788e8
Rafael Benzaquem Neto <rafaelbenzaquem@ulbra.edu.br>
2019-01-09 22:15:59 -0300

    Commit inicial app de mobilidade urbana

---
+++ app/src/main/java/com/rafaelbenzaquem/gpsapp/LocationData.java
@@ -0,0 +1,16 @@
+package com.rafaelbenzaquem.gpsapp;
+
+/**
+ * Created by evismar on 20/10/2017.
+ */
+
+public class LocationData {
+
+    double latitude;
+    double longitude;
+
+    public LocationData(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+}
